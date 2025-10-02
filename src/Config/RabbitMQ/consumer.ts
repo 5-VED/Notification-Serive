f1eb7b332998c77512b692448227b9dff1b286da
Ved Parmar <ved.parmar.official@gmail.com>
2025-10-02 15:40:33 +0530

    Refactor RabbitMQ consumer to ensure connection before consuming messages; remove unnecessary logging statements for improved clarity and maintainability.

--- src/Config/RabbitMQ/consumer.ts
+++ src/Config/RabbitMQ/consumer.ts
@@ -15,30 +15,26 @@ class RabbitMQConsumer extends RabbitMQConnectin {
         options: any = {}
     ): Promise<void> {
         try {
-            // await this.ensureConnection();
+            await this.ensureConnection();
             const channel = await this.connect();
 
             if (!channel) {
                 throw new Error(message.RABBITMQ_CHANNEL_NOT_AVAILABLE);
             }
-            console.log('consumeFromQueue  1 -------------->');
 
             // Ensure queue exists without redefining its arguments (avoids PRECONDITION_FAILED)
             await channel.checkQueue(queueName);
-            console.log('consumeFromQueue  2 -------------->');
             
             // Set prefetch count to process one message at a time
             await channel.prefetch(1);
             
-            console.log('consumeFromQueue  3 -------------->');
+
             
             // Start consuming
             await channel.consume(queueName, async (msg: any) => {
-                console.log('msg -------------->', msg);
                 if (msg) {
                     try {
-                        const messageContent = JSON.parse(msg.content.toString());
-                        console.log('messageContent -------------->', messageContent);
+                        const messageContent = JSON.parse(msg.content.toString());           
                         logger.info(`${message.RABBITMQ_MSG_RECEIVED_QUEUE} ${queueName}`);
 
                         // Process the message
